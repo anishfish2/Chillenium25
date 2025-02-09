28bf453de94cc4e5e9602f7f1b2e327e04caa9d5
Anish Karthik <89824626+anishfish2@users.noreply.github.com>
2025-02-09 12:47:06 -0600

    done

--- scripts/level_2.gd
+++ scripts/level_2.gd
@@ -4,7 +4,7 @@ extends Node2D
 
 # A flag to ensure we only transition once.
 var transitioned: bool = false
-var next_scene = preload("res://levels/level_3.tscn").instantiate()
+var next_scene = preload("res://scripts/ending.tscn").instantiate()
 
 
 
@@ -13,8 +13,8 @@ func _process(delta: float) -> void:
 	var player = $Player 
 	
 	if player.global_position.x > threshold_x and player.global_position.y > threshold_y:
-		get_node("/root/WinSound").play()
-		get_tree().change_scene_to_file("res://levels/tutorial3.tscn")
+		#get_node("/root/WinSound").play()
+		get_tree().change_scene_to_file("res://scripts/ending.tscn")
 	
 	if Input.is_action_just_pressed("ui_restart"):
 		get_node("/root/Restart").play()
